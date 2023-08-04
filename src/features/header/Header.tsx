867887727f0ff3f0aa32c41ad7eb93d2046c17c2
paskanov <v.paskanov@gmail.com>
2023-08-04 10:55:47 +0300

    Initial commit

---
+++ src/features/header/Header.tsx
@@ -0,0 +1,16 @@
+import s from "./styles.module.css"
+
+export const Header = () => {
+    return (
+        <div className={s.header}>
+            <div className={s.button}>
+        <button>CART</button>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
