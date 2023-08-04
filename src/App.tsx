06d4f888fad9f4e9a2be0c60a48165a8fd6c84dd
paskanov <v.paskanov@gmail.com>
2023-08-04 11:00:52 +0300

    Add some files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+ import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+ import {Header} from "./features/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,10 +1,15 @@
 import s from "./styles.module.css"
 
 export const Header = () => {
+
+    const addInCart = () => {
+        return alert('Add in a cart successfully')
+    }
+
     return (
         <div className={s.header}>
             <div className={s.button}>
-        <button>CART</button>
+        <button onClick={addInCart}>CART</button>
             </div>
         </div>
     )
