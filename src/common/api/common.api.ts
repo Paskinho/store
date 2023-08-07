ffd87e827e65917ba6507cd9d1b7bf17ed3a1d95
paskanov <v.paskanov@gmail.com>
2023-08-07 16:16:06 +0300

    Add API-KEY

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 export const instance = axios.create({
     baseURL:'',
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        "API-KEY": "AIzaSyAQ-BEh6rF56LnxMC9-74L6KY0jq1hWyL0"
+    }
 
 })
