d380f436771842932f7df861f3f98768602cf20c
paskanov <v.paskanov@gmail.com>
2023-08-08 16:57:35 +0300

    Add base URL to app (create back in firebase)

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL:'',
+    baseURL:'https://back-for-store-default-rtdb.firebaseio.com/',
     withCredentials: true,
     headers: {
         "API-KEY": "AIzaSyAQ-BEh6rF56LnxMC9-74L6KY0jq1hWyL0"
