ea726cb75ed65ad11a83cb1b843ff5fad6a01a55
paskanov <v.paskanov@gmail.com>
2023-08-09 10:53:09 +0300

    Add logic for loginization

--- src/features/auth/Login/Login.tsx
+++ src/features/auth/Login/Login.tsx
@@ -1,21 +1,30 @@
+import { toast } from "react-toastify";
 import {useAppDispatch} from "../../../app/hooks";
 import {authApi} from "../auth.api";
 
 export const Login = () => {
 
     const dispatch = useAppDispatch()
+    const navigate = useNavigate()
 
 
 
     const loginHandler = () => {
 
         const payload = {
-            email: "",
-            password: "",
+            email: "virgil@gmail.com",
+            password: "virgil",
             rememberMe: false
         }
 
-          dispatch(authApi.login(payload)).then(()=> {})
+          dispatch(authApi.login(payload)).then((res: any)=> {
+              toast.success("Successfully login!")
+              setTimeout(()=> {
+                  navigate('/')
+              }, 1000)
+          }).catch((err: any)=>{
+              toast.error(err.e.response.data.error)
+          })
 
     }
 
