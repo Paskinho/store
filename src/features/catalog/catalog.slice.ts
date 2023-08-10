8d1129dfd6e8bcffe7dc836b7f01e60dd1b7bc0b
paskanov <v.paskanov@gmail.com>
2023-08-10 16:49:59 +0300

    Create slice and API for catalog

---
+++ src/features/catalog/catalog.slice.ts
@@ -0,0 +1,12 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+const slice = createSlice({
+    name: 'catalog',
+    initialState: {},
+    reducers: {
+
+    },
+    extraReducers: {
+
+    }
+})
