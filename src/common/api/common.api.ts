a01db71189633018bd00b5017b4b05eefd16c164
paskanov <v.paskanov@gmail.com>
2023-08-29 11:52:01 +0300

    Refactoring with API

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,6 +4,7 @@ export const instance = axios.create({
     baseURL:'https://back-for-store-default-rtdb.firebaseio.com/',
     withCredentials: true,
     headers: {
-        "API-KEY": "AIzaSyAQ-BEh6rF56LnxMC9-74L6KY0jq1hWyL0"
+        "API-KEY": "AIzaSyAQ-BEh6rF56LnxMC9-74L6KY0jq1hWyL0",
+        "Access-Control-Allow-Origin": "*"
     }
 })
